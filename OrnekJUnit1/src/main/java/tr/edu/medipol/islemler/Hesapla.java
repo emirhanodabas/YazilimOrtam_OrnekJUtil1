339a2fcdf7f435e52aa45e788396e917b731c904
emirhanodabas <emirhan.eo249@gmail.com>
2022-05-21 01:33:28 +0300

    Jutil
    JUtil

---
+++ OrnekJUnit1/src/main/java/tr/edu/medipol/islemler/Hesapla.java
@@ -0,0 +1,37 @@
+package tr.edu.medipol.islemler;
+
+public class Hesapla {
+	
+	static int topla(int sayi1,int sayi2){
+			
+		return sayi1+sayi2;
+	}
+	static int topla(int ... sayilar){
+		int toplamsonuc=0;
+		for (int sayi : sayilar) {
+			toplamsonuc +=sayi;
+		}
+		return toplamsonuc;
+	}
+	static int cikar(int ... sayilar){
+		int cikansonuc=0;
+		for (int sayi : sayilar) {
+			cikansonuc =cikansonuc-sayi;
+		}
+		return cikansonuc;
+	}
+	static int carp(int ... sayilar){
+		int carpilansonuc=1;
+		for (int sayi : sayilar) {
+			carpilansonuc =carpilansonuc*sayi;
+		}
+		return carpilansonuc;
+	}
+	public static void main(String[] args) {
+		
+		int sonuc=topla(243,654);
+		System.out.println("243+654 = "+sonuc);
+	
+	}
+
+}
---
+++ OrnekJUnit1/src/test/java/tr/edu/medipol/islemler/HesaplaTest.java
@@ -0,0 +1,59 @@
+package tr.edu.medipol.islemler;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HesaplaTest {
+
+	private Hesapla fixture=new Hesapla();
+
+	@Test
+	public void testikiSayiTopla() {
+		int sayi1=435;
+		int sayi2=534;
+		int beklenesonuc=969;
+		
+		int sonuc= fixture.topla(sayi1,sayi2);
+		
+		assertEquals(beklenesonuc,sonuc);
+	}
+	@Test
+	public void testdortSayiTopla() {
+		int sayi1=435;
+		int sayi2=534;
+		int sayi3=1;
+		int sayi4=2;
+		int beklenesonuc=972;
+		
+		int sonuc= fixture.topla(sayi1,sayi2,sayi3,sayi4);
+		
+		assertEquals(beklenesonuc,sonuc);
+	}
+	@Test
+	public void testdortSayiCikar() {
+		int sayi1=2;
+		int sayi2=1;
+		int sayi3=234;
+		int sayi4=500;
+		int beklenesonuc=-737;
+		
+		int sonuc= fixture.cikar(sayi1,sayi2,sayi3,sayi4);
+		
+		assertEquals(beklenesonuc,sonuc);
+	}
+
+	@Test
+	public void testdortSayiCarp() {
+		int sayi1=5;
+		int sayi2=4;
+		int sayi3=3;
+		int sayi4=2;
+		int beklenesonuc=120;
+		
+		int sonuc= fixture.carp(sayi1,sayi2,sayi3,sayi4);
+		
+		assertEquals(beklenesonuc,sonuc);
+	}
+
+}
